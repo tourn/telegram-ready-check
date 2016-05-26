1c2d7e763dd38be52bd522ba77bb5b00402ae57d
tourn <latzer.daniel@gmail.com>
2016-05-26 22:45:11 +0200

    LET THERE BE LIGHT

---
+++ bot.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Basic example for a bot that awaits an answer from the user. It's built upon
+# the state_machine_bot.py example
+# This program is dedicated to the public domain under the CC0 license.
+
+import logging
+from telegram import Emoji, ForceReply, InlineKeyboardButton, \
+    InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, \
+    CallbackQueryHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.DEBUG)
+
+import properties
+
+# Define the different states a chat can be in
+MENU, AWAIT_CONFIRMATION, AWAIT_INPUT = range(3)
+
+# Python 2 and 3 unicode differences
+try:
+    YES, NO = (Emoji.THUMBS_UP_SIGN.decode('utf-8'), Emoji.THUMBS_DOWN_SIGN.decode('utf-8'))
+except AttributeError:
+    YES, NO = (Emoji.THUMBS_UP_SIGN, Emoji.THUMBS_DOWN_SIGN)
+
+NOW, SOON, LATER, NEVER = "now", "soon", "later", "never"
+
+# States are saved in a dict that maps chat_id -> state
+last = dict()
+state = dict()
+# Sometimes you need to save data temporarily
+context = dict()
+# This dict is used to store the settings value for the chat.
+# Usually, you'd use persistence for this (e.g. sqlite).
+values = dict()
+
+reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(NOW, callback_data=NOW),
+                                        InlineKeyboardButton(SOON, callback_data=SOON)],
+                                        [InlineKeyboardButton(LATER, callback_data=LATER),
+                                        InlineKeyboardButton(NEVER, callback_data=NEVER)
+                                            ]])
+
+def ready_check(bot, update):
+    chat_id = update.message.chat_id
+    user_id = update.message.from_user.id
+    state[chat_id] = dict()
+    bot.sendMessage(chat_id, text="Are you ready?", reply_markup=reply_markup)
+
+
+def confirm_value(bot, update):
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    user_id = query.from_user.id
+    text = query.data
+    user_state = state.get(user_id, MENU)
+    user_context = context.get(user_id, None)
+
+    print('chat ' + str(chat_id))
+    print('user ' + str(user_id))
+    #bot.answerCallbackQuery(query.id, text="Ok!")
+    state[chat_id][user_id] = text
+    #values[user_id] = user_context
+    bot.editMessageText(text="Changed value to %s." % text,
+                        chat_id=chat_id,
+                        message_id=query.message.message_id,
+                        reply_markup=reply_markup)
+
+def help(bot, update):
+    bot.sendMessage(update.message.chat_id, text="Use /set to test this bot.")
+
+
+def error(bot, update, error):
+    logging.warning('Update "%s" caused error "%s"' % (update, error))
+
+# Create the Updater and pass it your bot's token.
+updater = Updater(properties.TELEGRAM_TOKEN)
+
+# The command
+updater.dispatcher.add_handler(CommandHandler('ready', ready_check))
+# The confirmation
+updater.dispatcher.add_handler(CallbackQueryHandler(confirm_value))
+updater.dispatcher.add_handler(CommandHandler('start', help))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_error_handler(error)
+
+# Start the Bot
+updater.start_polling()
+
+# Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+# SIGTERM or SIGABRT
+updater.idle()
